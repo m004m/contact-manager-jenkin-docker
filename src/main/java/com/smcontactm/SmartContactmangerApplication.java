7500bb82043bcf0ac9b993a3bf550017c380fc6c
Mahendra Kharvi <mahi.m004@gmail.com>
2024-02-07 18:53:54 +0530

    logger added for thr application

--- src/main/java/com/smcontactm/SmartContactmangerApplication.java
+++ src/main/java/com/smcontactm/SmartContactmangerApplication.java
@@ -1,12 +1,16 @@
 package com.smcontactm;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SmartContactmangerApplication {
 
+	public static final Logger LOGGER = LoggerFactory.getLogger(SmartContactmangerApplication.class);
 	public static void main(String[] args) {
+		LOGGER.info("Application started----------------------------------------->");
 		SpringApplication.run(SmartContactmangerApplication.class, args);
 	}
 
